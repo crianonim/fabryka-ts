2bc21442f67a03434a3b2937c0e8c70b9f5969fe
Jan Skowronski <crianonim@gmail.com>
2019-11-28 10:28:55 +0000

    Playing with the miner type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import * as types from './types'
 const e1=new types.Entity
 const e2=new types.Entity
-const entities=[e1,e2];
+const m1=new types.Miner
+const entities=[e1,e2,m1];
+entities.map(e=>console.log(e.getId(),e.typename))
 
-entities.map(e=>console.log(e.getId()))
+const tick=()=>{
+    entities
+    .filter(e=>e instanceof types.DoerEntity)
+    .forEach( e => (e as types.DoerEntity).do())
+}
+for (let i=0;i<10;i++) tick();
--- src/types.ts
+++ src/types.ts
@@ -10,6 +10,9 @@ export class Entity {
     }
     typename="Entity"
 }
+export class Inventory {
+    slots=Array<Item>()
+}
 export interface Item {
     stackable:boolean;
     amount:number;
@@ -18,13 +21,47 @@ export interface Item {
 export interface Doer {
     do(): void
 }
+export class OreItem implements Item {
+    stackable=true;
+    amount=1;
+    typeName="OreItem";
+    constructor(amount=1){
+        this.amount=amount
+    }
+}
+
+
+export class DoerEntity extends Entity {
+    do(){
+        console.log("DoerEntet")
+    }
+}
+
+
+export class Miner extends DoerEntity implements Machine,Producer{
+    typename="Miner";
+    out=new Inventory;
+    stage=0;
+    maxStage=3;
+    do(){
+        console.log("Miner working",this.out, this.stage,"/",this.maxStage)
+        this.stage++;
+        if (this.stage===this.maxStage){
+            this.stage=0;
+            console.log("Mines")
+            this.out.slots.push(new OreItem(1))
+        }
+    }
+}
 
 export interface Machine extends Doer {
 
 }
 
 export interface Producer {
-
+    out:Inventory;
+    stage:number;
+    maxStage:number
 }
 
 export interface EnergyConsumer {
